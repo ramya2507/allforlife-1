14e535919c863dc54058011afcbe0f153019ab61
MegaMind <ramyaa1431@gmail.com>
2021-02-02 14:19:47 -0500

    move data into seeds

--- server/db/schema/development.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO customers(first_name, last_name, email, password)
-VALUES ('Eva' ,'Stanley', 'kelez@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Louisa', ' Myers', 'getsd@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Bells', ' corner', 'bells@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('terry', ' waya', 'terry@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('kerry' ,' washington', 'kerry@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('olivia', ' pope', 'olivia@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');

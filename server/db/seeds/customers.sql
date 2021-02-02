65877fd25f6e26e11725fdae76bf450f6759f2fc
MegaMind <ramyaa1431@gmail.com>
2021-02-02 14:34:05 -0500

    test data for customers

---
+++ server/db/seeds/customers.sql
@@ -0,0 +1,7 @@
+INSERT INTO customers(first_name, last_name, email, password)
+VALUES ('Eva' ,'Stanley', 'kelez@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Louisa', ' Myers', 'getsd@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Bells', ' corner', 'bells@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('terry', ' waya', 'terry@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('kerry' ,' washington', 'kerry@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('olivia', ' pope', 'olivia@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');

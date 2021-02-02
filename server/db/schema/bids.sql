7668fc309db9ec79ef7f5f6eb7ecfbd70fdde956
MegaMind <ramyaa1431@gmail.com>
2021-02-02 18:20:37 -0500

    delete the mid lookup for providers and bids

--- server/db/schema/bids.sql
+++ server/db/schema/bids.sql
@@ -2,6 +2,11 @@ DROP TABLE IF EXISTS bids CASCADE;
 
 CREATE TABLE bids (
   id SERIAL PRIMARY KEY NOT NULL,
-  provider_bid_id INTEGER REFERENCES provider_bids(id) ON DELETE CASCADE,
+  description TEXT,
+  fee VARCHAR NOT NULL,
+  is_hourly BOOLEAN,
+  is_complete_treatment BOOLEAN,
+  available_time VARCHAR,
+  provider_id INTEGER REFERENCES providers(id) ON DELETE CASCADE,
   job_posting_id INTEGER REFERENCES job_postings(id) ON DELETE CASCADE
 );

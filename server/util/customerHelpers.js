5de1003e0269faf8891a782ca1739340485b3261
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-02-03 13:13:20 -0500

    created function to get user by email and to add new user to database

---
+++ server/util/customerHelpers.js
@@ -0,0 +1,33 @@
+//function to get user with email from the database
+const getUserWithEmail = (email, db) => {
+  return db.query(`
+    SELECT * 
+    FROM customers
+    WHERE email = $1;
+  `, [email])
+    .then(res => {
+      return res.rows[0];
+    })
+    .catch(e => {
+      return null;
+    });
+};
+
+const addUser = (userData, db) => {
+  const {prefix, firstName, lastName, email, password } = userData;
+  return db.query(`
+  INSERT INTO customers (prefix, first_name, last_name, email, password)
+    VALUES($1, $2, $3, $4, $5)
+    RETURNING *;
+    `, [prefix, firstName, lastName, email, password])
+    .then(res => {
+      return res.rows[0];
+    })
+    .catch(e => {
+      return null;
+    });
+};
+module.exports = {
+  getUserWithEmail,
+  addUser
+};

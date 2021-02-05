516ba439a714df2df679ca6eb44571c33cea8df6
RAMYA ADIMOOLAM <35279844+ramya2507@users.noreply.github.com>
2021-02-04 19:15:49 -0500

    Revert "Feature/header"

--- server/db/dbIndex.js
+++ server/db/dbIndex.js
@@ -1,21 +1,13 @@
 const pg = require('pg');
-// load .env data into process.env
-require('dotenv').config();
-
-const connectionString = `postgres://development:${process.env.DB_USER}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 
 const client = new pg.Client({
-  connectionString: connectionString,
-})
-/*const client = new pg.Client({
   user:"development",
   password: "development",
   database: "allforlife",
   host: "localhost",
   port: 5432
-});*/
+});
 
-client.connect()
-.catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+client.connect().catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
 
 module.exports = client;

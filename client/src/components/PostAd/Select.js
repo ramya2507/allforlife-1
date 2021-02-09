5d9dbbd6668fee73378d8adb8e51285200e4ee6b
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-02-09 16:30:00 -0500

    added the select component

---
+++ client/src/components/PostAd/Select.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default function Select(props){
+  console.log(props, "props here")
+  const { heading,listArray,name} = props;
+  return(
+    <>
+    <h5>{heading}</h5>
+    <select name={name} onChange={props.handleChange}>
+     <option>{props.filter}</option>
+     {listArray.map((item,index) => {return <option key={index} value={item}> {item} </option>})}
+     </select>
+    </>
+  )
+ 
+}

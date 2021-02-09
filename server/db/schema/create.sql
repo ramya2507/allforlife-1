a40c0e0fcfe7d9898297fbbfcb9ab723d700f72a
MegaMind <ramyaa1431@gmail.com>
2021-02-09 18:48:45 -0500

    add job posting table to database

--- server/db/schema/create.sql
+++ server/db/schema/create.sql
@@ -21,6 +21,41 @@ CREATE TABLE providers(
     password VARCHAR(255) NOT NULL
 );
 
+DROP TABLE IF EXISTS job_postings CASCADE;
 
+CREATE TABLE job_postings(
+    id SERIAL PRIMARY KEY NOT NULL,
+    customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
+    appointmentFor VARCHAR(255),
+    description TEXT,
+    therapy VARCHAR(255),
+    sexuality VARCHAR(255),
+    age VARCHAR(255),
+    language VARCHAR(255),
+    ethnicity VARCHAR(255),
+    faith VARCHAR(255),
+    typeOfPayment VARCHAR(255),
+    maxPrice VARCHAR(255),
+    minPrice VARCHAR(255),
+    appointmentFrequency VARCHAR(255),
+    timeRequirement VARCHAR(255),
+    availabilityFrom VARCHAR(255),
+    availabilityTo VARCHAR(255),
+    postCreationTime TIMESTAMPTZ
+)
 
+DROP TABLE IF EXISTS symptomes CASCADE;
 
+CREATE TABLE symptomes(
+    id SERIAL PRIMARY KEY NOT NULL,
+    job_posting_id INTEGER REFERENCES job_postings(id) ON DELETE CASCADE,
+    name VARCHAR(255)
+)
+
+DROP TABLE IF EXISTS symptomes_look_up CASCADE;
+
+CREATE TABLE symptomes_look_up(
+    id SERIAL PRIMARY KEY NOT NULL,
+    job_posting_id INTEGER REFERENCES job_postings(id) ON DELETE CASCADE,
+    symptome_id INTEGER REFERENCES symptomes(id) ON DELETE CASCADE
+)

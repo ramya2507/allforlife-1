423977d26ee03a48d46335c1fca932c40dd608d7
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-02-10 20:40:55 -0500

    added the create new function

---
+++ server/util/jobPostHelpers.js
@@ -0,0 +1,42 @@
+//function to create a new job post
+
+const createNewPost = (jobPostObj, db) => {
+  
+  //replace empty values with null
+  for (const post in jobPostObj) {
+    //console.log(jobPostObj[post]);
+    if (jobPostObj[post] === "") {
+      jobPostObj[post] = null;
+    }
+  }
+  console.log(jobPostObj);
+  const {appointmentFor, description, symptomes, insurance,  therapy, age, sexuality, language, ethnicity, faith, typeOfPayment, maxPrice, minPrice, appointmentFrequency, timeRequirement, availabilityFrom, availabilityTo } = jobPostObj;
+  //console.log(symptomes, "hello")
+  const customerId = 2;
+  return db.query(`
+  INSERT INTO job_postings (customer_id, appointmentFor, description, therapy, sexuality, age, language, ethnicity, faith, typeOfPayment, maxPrice, minPrice, appointmentFrequency, timeRequirement, availabilityFrom, availabilityTo, insurance)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
+  RETURNING id as job_posting_id;
+  `, [customerId, appointmentFor, description, therapy, sexuality, age, language, ethnicity, faith, typeOfPayment, maxPrice, minPrice, appointmentFrequency, timeRequirement, availabilityFrom, availabilityTo, insurance])
+    .then(res => {
+      const jobPostingId = res.rows[0].job_posting_id;
+      //console.log(jobPostingId, "1st")
+      symptomes.forEach(symptome => {
+        return db.query(`
+          INSERT INTO symptomes_look_up(job_posting_id, symptome_id)
+          VALUES(${jobPostingId}, $1)
+          RETURNING *;
+        `, [symptome])
+          .then(res => {
+            return res.rows[0];
+          })
+          .catch(res => {
+            return null;
+          });
+      });
+    });
+};
+
+module.exports = {
+  createNewPost
+};

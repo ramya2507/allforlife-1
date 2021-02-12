20f5a3df2fb750f4c0cddc79ec23bd06da60e4cf
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-02-11 19:31:49 -0500

    added the header routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,48 @@
-import react , {useState} from "react";
-import './App.css';
-import Footer from  "./Footer";
-import Header from "./Header";
-import PostAd from "./PostAd";
+import React from 'react';
+import { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './Header';
+import Footer from './Footer';
 
+import Login from './customer/Login';
+import Register from './customer/Register';
+import PostAd from './PostAd';
+import LoginDecision from './LoginDecision';
+import RegisterDecision from './RegisterDecision';
 
-import Login from "./customer/Login"
-import Register from "./customer/Register";
-
+/*  <>
+   <Header />
+   {!user && <Register setUser={setUser}/>}
+   {user && <h1>Hi {user.username} !</h1>}
+   </> */
 
 function App() {
-
-const [user,setUser]= useState(null);
+  const [user,setUser]= useState(null);
 
   return (
-  <>
-    <Header />
-    {!user && <PostAd setUser={setUser}/>}
-    {user && <h1>Hi {user.first_name} !</h1>}
-    <Footer />
-  </> );
+    <Router>
+      <Header user={user} setUser={setUser}/>
+      <Switch>
+        <Route path="/customerlogin">
+          <Login setUser={setUser} /> 
+        </Route>
+        <Route path="/customerregister">
+          <Register setUser={setUser} />
+        </Route>
+        <Route path="/register">
+          {!user && <RegisterDecision setUser={setUser} />}
+        </Route>
+        <Route path="/login">
+          {!user && <LoginDecision setUser={setUser} />}
+        </Route>
+        <Route path="/" exact>
+          <PostAd  user={user}/>
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  
+  );
 }
 
 export default App;

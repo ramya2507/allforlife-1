f1f19ebf21f056d5d7f39b52303fa81e9014073d
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-02-11 19:41:34 -0500

    added the username field

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,17 +2,35 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 
-const { getUserWithEmail, addUser } = require('../util/customerHelpers');
+const { getUserWithUserName, addUser } = require('../util/customerHelpers');
 
 //api routes
 
 module.exports = (db) => {
+  
+  router.get('/', async(req, res) => {
+    try {
+      const allUsers = await db.query(`SELECT * FROM customers`);
+      res.json(allUsers.rows);
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
+  
+  router.get('/:id', (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if (!user) {
+      res.status(404).send("this user does not exist");
+    }
+    res.send(user);
+  });
+  
   //registration route
   router.post('/register', (req, res) => {
-    const { prefix, firstName, lastName, email, password } = req.body;
+    const { prefix, firstName, lastName, userName, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 12);
   
-    if (!firstName || !lastName || !email || !password) {
+    if (!firstName || !lastName ||!userName ||!email || !password) {
       res.send("You are missing a field");
       return;
     }
@@ -20,18 +38,19 @@ module.exports = (db) => {
       prefix: prefix,
       firstName: firstName,
       lastName:lastName,
+      userName:userName,
       email:email,
       password: hashedPassword
     };
-    getUserWithEmail(email, db)
+    getUserWithUserName(userName, db)
       .then(response => {
         if (response) {
-          res.send({error: "An account with this email exist"});
+          res.json([]);
           return;
         }
         addUser(userData, db).then(newUser => {
-          req.session.customerId = newUser.id;
-          console.log(req.session.customerId);
+          req.session.customerId = newUser .id;
+          console.log(req.session.customerId, "user");
           res.json(newUser);
           return;
         })
@@ -39,23 +58,23 @@ module.exports = (db) => {
           
       })
       .catch(e => {
-        res.status(500).json({ error: e.message});
+        //res.status(500).json({ error: e.message});
       });
   });
   
   //login route
   router.post('/login', (req, res) => {
-    const {email, password} = req.body;
-    getUserWithEmail(email, db)
+    const {userName, password} = req.body;
+    getUserWithUserName(userName, db)
       .then(user => {
         if (!user) {
-          res.send("Email does not exist");
+          res.json([]);
         } else if (!bcrypt.compareSync(password, user['password'])) {
-          res.send("Password is incorrect");
+          res.json([]);
           return;
         }
         req.session.customerId = user.id;
-        //console.log(req.session.customerId);
+        console.log(req.session.customerId);
         res.json(user);
       })
       .catch(e => {
@@ -66,10 +85,4 @@ module.exports = (db) => {
   });
   return router;
 };
-
-
-
-
-
-
 

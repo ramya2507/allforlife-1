eeb4da1ac9e0c75fae9608018ef754215f6666f6
MegaMind <ramyaa1431@gmail.com>
2021-02-18 02:13:13 -0500

    provider dashboard

--- client/src/components/provider/ProviderDashboard.js
+++ client/src/components/provider/ProviderDashboard.js
@@ -1,6 +1,25 @@
-
+import "./ProviderDashboard.css";
+import {useState, useEffect} from "react";
+import axios from "axios";
+import ProposalAd from "../ProposalAd";
 
 export default function ProviderDashboard (props) {
 
-  return <div>I am ProviderDashboard</div>;
+  return (
+    <>   
+    <section className="profile-container" >
+        <div className="profile-container-title">
+            Hello, {props.user.userName}
+        </div>
+        <div className="profile-separation"></div>
+        <div className="profile-proposals-container">
+            <div className="profile-proposals">
+                <h3>Look for opportunities <span className="yellow-icon">◣</span> </h3>
+                <ProposalAd />
+            </div>
+        </div>
+    </section>
+    </>
+)
+
 }
